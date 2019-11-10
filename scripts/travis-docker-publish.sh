8c0aacb7928598e2ad1492b6cfc9e34187611534
Nick Cromwell <cromwellnick@gmail.com>
2019-11-09 23:42:26 -0500

    Bump major version - 2.*

--- scripts/travis-docker-publish.sh
+++ scripts/travis-docker-publish.sh
@@ -4,7 +4,7 @@ DOCKER_TAG=''
 if [ $TRAVIS_EVENT_TYPE != "pull_request" ]; then
   case "$TRAVIS_BRANCH" in
     "master")
-      DOCKER_TAG=1.0.$TRAVIS_BUILD_NUMBER
+      DOCKER_TAG=2.0.$TRAVIS_BUILD_NUMBER
       ;;
     "dev")
       DOCKER_TAG=dev
