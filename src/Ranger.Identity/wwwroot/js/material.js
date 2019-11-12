adbba82a8dc37f95bc8843bfce44a7149087f209
Nick Cromwell <cromwellnick@gmail.com>
2019-11-12 01:06:01 -0500

    remove material.js

--- src/Ranger.Identity/wwwroot/js/material.js
+++
@@ -1,6 +0,0 @@
-import * as mdc from '../lib/material-components-web/material-components-web.min.js';
-// for accessing is as a window object
-window.mdc = mdc;
-
-// Or do this inside component
-document.addEventListener('DOMContentLoaded', () => mdc.autoInit());
