2f1a5fe58c6fb0b269a0a76de6c39d27f1f8918c
Nick Cromwell <cromwellnick@gmail.com>
2020-08-22 13:18:18 -0400

    ProjectId -> Id; IntegrationId -> Id

--- src/Ranger.Identity/Models/Internal/ProjectModel.cs
+++ src/Ranger.Identity/Models/Internal/ProjectModel.cs
@@ -4,7 +4,7 @@ namespace Ranger.Identity
 {
     class ProjectModel
     {
-        public Guid ProjectId { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 }
