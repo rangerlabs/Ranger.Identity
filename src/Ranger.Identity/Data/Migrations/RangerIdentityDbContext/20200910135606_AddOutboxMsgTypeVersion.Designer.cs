fcd7af3006460778e6f55879eff6f1e156347746
Nick Cromwell <cromwellnick@gmail.com>
2020-09-10 10:07:03 -0400

    Add Message Type and Version columns

--- src/Ranger.Identity/Data/Migrations/RangerIdentityDbContext/20200910135606_AddOutboxMsgTypeVersion.Designer.cs
+++ src/Ranger.Identity/Data/Migrations/RangerIdentityDbContext/20200910135606_AddOutboxMsgTypeVersion.Designer.cs
@@ -2,16 +2,18 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Ranger.Identity.Data;
 
-namespace Ranger.Identity
+namespace Ranger.Identity.Migrations.RangerIdentityDb
 {
     [DbContext(typeof(RangerIdentityDbContext))]
-    partial class RangerIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200910135606_AddOutboxMsgTypeVersion")]
+    partial class AddOutboxMsgTypeVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
